1ea3fa10cabf59701ea9041c56ed85b2749fa45e
awesomedelight <77675727+awesomedelight@users.noreply.github.com>
2021-04-05 19:08:43 +0100

    Update settings.py

--- securedesign/securedesign/settings.py
+++ securedesign/securedesign/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2^ikc%w9m8auhq2q@t3wm&!^$&4n0(w12@-ch#q0^)lt@-xs$h'
+SECRET_KEY = 'do not think I exposed this'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
